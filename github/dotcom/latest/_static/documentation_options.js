a28bd5d646db3adfc410c3384e12a8c61950e567
ShineyDev <30989490+ShineyDev@users.noreply.github.com>
2021-05-17 19:21:39 +0000

    update docs for ShineyDev/github

---
+++ github/dotcom/latest/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '1.0.0.0a1+g2746d5e',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'dirhtml',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
