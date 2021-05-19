601f42884538e4a1022084f835f2a575df689946
ShineyDev <30989490+ShineyDev@users.noreply.github.com>
2021-05-19 19:26:15 +0000

    update docs for ShineyDev/graphql

--- graphql/latest/_static/documentation_options.js
+++ graphql/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0a1+g79a6047',
+    VERSION: '0.1.0a1+gae52fd7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
