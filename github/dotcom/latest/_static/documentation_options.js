42fda55912271aa1d84175e3c962eb3797fdc941
ShineyDev <30989490+ShineyDev@users.noreply.github.com>
2021-06-14 16:22:58 +0000

    update docs for ShineyDev/github

--- github/dotcom/latest/_static/documentation_options.js
+++ github/dotcom/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0.0a1+g71e4820',
+    VERSION: '1.0.0.0a1+geed0768',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
