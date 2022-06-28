0b671894a6c89e305ad1f7050d3633fe4172cd3a
ShineyDev <30989490+ShineyDev@users.noreply.github.com>
2022-06-28 14:36:29 +0000

    update docs for ShineyDev/pretty

--- pretty/latest/_static/documentation_options.js
+++ pretty/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.0.0a1+g358223f',
+    VERSION: 'v2.0.0a1+gd8fd1b0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
