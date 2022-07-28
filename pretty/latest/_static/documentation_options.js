03b5568bb6c142c74a51262b2c3ab21004cf9d82
ShineyDev <30989490+ShineyDev@users.noreply.github.com>
2022-07-28 15:26:51 +0000

    update docs for ShineyDev/pretty

--- pretty/latest/_static/documentation_options.js
+++ pretty/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.0.0a1+g9e8870a',
+    VERSION: 'v2.0.0a1+g2f0d17f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
@@ -10,5 +10,5 @@ var DOCUMENTATION_OPTIONS = {
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: false,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
